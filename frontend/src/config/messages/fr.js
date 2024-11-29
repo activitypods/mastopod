9267a1c8d44d6dece01653fc474d9b7442d7bf89
srosset81 <srosset81@gmail.com>
2024-11-29 10:55:43 +0100

    New EditProfileButton which redirects to new page

--- frontend/src/config/messages/fr.js
+++ frontend/src/config/messages/fr.js
@@ -4,7 +4,9 @@ export default {
   app: {
     description: 'Une appli compatible Mastodon qui enregistre tout dans votre Pod',
     action: {
-      edit_profile: 'Editer le profile',
+      edit_profile: 'Éditer mon profil',
+      edit_public_profile: 'Éditer le profil public',
+      edit_private_profile: 'Éditer le profil privé',
       follow: 'Suivre',
       unfollow: 'Ne plus suivre',
       send: 'Envoyer',
@@ -44,7 +46,7 @@ export default {
       post_like_removed: 'Le like du message a été enlevé',
       actor_followed: 'Vous suivez maintenant cet acteur',
       actor_unfollowed: 'Vous ne suivez plus cet acteur',
-      image_upload_error: 'Echec de l\'upload de l\'image',
+      image_upload_error: "Échec de l'upload de l'image"
     },
     validation: {}
   }
